cb746d78ec0c775686ca7fa4e31ee30f75f3234d
Nguyen Thi Nga <nga.nguyen21@pn.com.vn>
2020-07-25 16:01:50 +0700

    Submit todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      todoItems:[
+        {title: 'Buy Snacks', isComplete:true},
+        {title: 'Go swimming', isComplete:true},
+        {title:'Take a dog for a walk' }
+        
+      ]
+
+    } 
+  }
+  onItemClicked(item){
+   
+    return(event)=>{
+      const isComplete = item.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+
+      this.setState({
+        todoItems:[
+          ...todoItems.slice(0,index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    };
+
+  }
+
+  render(){
+    const {todoItems} =this.state;
+    if(todoItems.length){
+    return (
+      <div className="App">
+        {
+          todoItems.length && todoItems.map((item,index)=>
+          <TodoItem 
+            key={index} 
+            item={item}
+            onClick={this.onItemClicked(item)}
+          />
+          )
+        }
+          {/* <TodoItem title="Buy Snacks" />
+          <TodoItem  title="Go swimming"/>
+          <TodoItem  title = "Take a dog for a walk"/> */}
+        {
+           todoItems.length == 0 && 'Nothing Here'
+        }
+      </div>
+    );
+
+    
+
+  }
 }
 
+}
 export default App;
+
---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+
+import './TodoItem.css';
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+class TodoItem extends Component{
+    render(){
+        const { item, onClick } = this.props;
+        return(
+           
+            <div onClick={onClick} className={classNames('TodoItem',{
+                'TodoItem-complete': item.isComplete
+            })}>
+            
+              <p>{this.props.item.title}</p>
+                
+            </div>
+
+        );
+    }
+}
+export default TodoItem;
